e6214d1e561ed9612c9c9d31e9747f159dac97e5
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-02 16:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function threeSum(arr, target) {
 // write your code here
+	let sum =0;
+	for(let i=0; i<arr.length; i++){
+		if(arr[i] == 1){
+			let target = arr[i];
+			sum = arr[i-1]+ target + arr[i+1]; 
+		}
+	return sum;
+		
+	}
+	
   
 }
 
